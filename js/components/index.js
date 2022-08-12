bb53e54c285a7b3ab6591b5e7d6238055597a3c2
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-08-12 14:57:02 +0530

    feat: added helper functions to get child todos recursively, updated styles

---
+++ js/components/index.js
@@ -0,0 +1,5 @@
+export { default as createTodoBase } from "./todoBase.js"
+export { default as createTodoRoot } from "./todoRoot.js"
+export { default as createTodoInput } from "./todoInput.js"
+export { default as createTodoCheckbox } from "./todoCheckbox.js"
+export { default as createTodoButton } from "./todoButton.js"
--- js/utils/helperfuncs.js
+++ js/utils/helperfuncs.js
@@ -6,4 +6,32 @@ const validateInput = (value = "") => {
   return true
 }
 
-export { validateInput }
+const getFilteredTodos = (todos = [], todoId) => {
+  const result = []
+  todos.forEach((todoItem) => {
+    if (todoItem?.children?.length > 0) {
+      const subArr = getFilteredTodos(todoItem.children, todoId)
+      todoItem.children = subArr
+    }
+    if (todoItem.id !== todoId) {
+      result.push(todoItem)
+    }
+  })
+  return result
+}
+
+const findTodo = (todos = [], todoId) => {
+  let searchedTodo
+
+  todos.forEach((todoItem) => {
+    if (!searchedTodo && todoItem?.children?.length > 0) {
+      searchedTodo = findTodo(todoItem.children, todoId)
+    } else {
+      searchedTodo = todoItem
+    }
+  })
+
+  return searchedTodo
+}
+
+export { validateInput, getFilteredTodos, findTodo }
